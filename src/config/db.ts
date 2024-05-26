bea0bc3f88a6ecdc4ed556d11fa47b0ac8a95d44
IgorTerriaga <igsantos1996@gmail.com>
2024-05-26 16:40:12 -0300

    add example .env

---
+++ src/config/db.ts
@@ -0,0 +1,8 @@
+import { config } from 'dotenv';
+import {connect} from 'mongoose';
+
+
+const connectToMongoDB = async() => {
+    config()
+    await connect(process.env.MONGODB_CONNECT_URL ?? 'mongodb://localhost/candles')
+}
